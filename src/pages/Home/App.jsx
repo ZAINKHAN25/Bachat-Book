6a481055bdf50b44e8fa6156487a5db642141b12
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-08-30 20:37:17 +0500

    Again Creating Some Structure.

--- src/pages/Home/App.jsx
+++ src/pages/Home/App.jsx
@@ -1,9 +1,19 @@
 import { Box } from "@chakra-ui/react";
+import { useState } from "react";
+
+import HomeComp from "../../components/HomeComp/index.jsx";
+import LandingComp from "../../components/LandingComp/index.jsx";
 
 function App() {
+
+  let [userLogin, setUserLogin ] = useState(true);
+
   return (
     <Box>
       Bachat Book (بچت بُک)
+      {
+        userLogin ? <HomeComp /> : <LandingComp />
+      }
     </Box>
   );
 }
